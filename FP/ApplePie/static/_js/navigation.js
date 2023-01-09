1497d81081b60ded22bc1093f8505899e294180a
khj33 <hyunjo111@naver.com>
2023-01-09 17:53:35 +0900

    javascript

---
+++ FP/ApplePie/static/_js/navigation.js
@@ -0,0 +1,9 @@
+// 네이게이션 open 함수
+function openNav() {
+    document.getElementById("mySidenav").style.width = "220px";
+}
+
+// 네이게이션 close 함수
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
