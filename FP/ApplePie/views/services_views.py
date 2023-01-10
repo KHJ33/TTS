93a91ce6a25c41d68376a7c76da4bc3bf814c855
khj33 <hyunjo111@naver.com>
2023-01-10 23:46:45 +0900

    services py

---
+++ FP/ApplePie/views/services_views.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, url_for, render_template, flash, request, session, g
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import redirect
+
+from ApplePie import db
+from ApplePie.forms import UserCreateForm, UserLoginForm
+from ApplePie.models import User
+
+bp = Blueprint('model', __name__, url_prefix='/model')
+
+@bp.route('/service/', methods=('GET', 'POST'))
+def service():
+    if request.method == 'POST' and request.form['btn']== 'saveImg':
+        from ..AI_Model import tesseract
+
+        ocr = tesseract.OCR('test.PNG')
+        text = ocr.ImgToText()
+
+        print(text)
+
+        return render_template('services/services.html', text = text)
+
+    elif request.method == 'POST' and request.form['btn']== 'ToTTS' :
+
+        text = request.form['textarea']
+
+        return render_template('services/services.html', text = text)
+    
+    
+    return render_template('services/services.html')
