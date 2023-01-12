e9c9250f0af6a29157dc6124aaba0c72f6eee11a
khj33 <hyunjo111@naver.com>
2023-01-12 09:44:25 +0900

    add PATH

--- FP/config.py
+++ FP/config.py
@@ -11,5 +11,15 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # 암호화 처리 옵션
 SECRET_KEY = "dev"
 
+# tersseract.py 경로 설정 부분
 TESSERACT_PATH = './ApplePie/package/Tesseract-OCR/tesseract.exe'
 IMAGE_PATH = 'ApplePie/saveImg/'
+
+# tts.py 경로 설정 부분
+GLOW_TTS_CHECKPOINT_PATH = './ApplePie/AI_Model/glow_tts/checkpoint_39000.pth.tar'
+GLOW_TTS_CONFIG_PATH = './ApplePie/AI_Model/glow_tts/config.json'
+
+HIFI_GAN_CHECKPOINT_PATH = './ApplePie/AI_Model/hifi_gan/checkpoint_300000.pth.tar'
+HIFI_GAN_CONFIG_PATH = './ApplePie/AI_Model/hifi_gan/config.json'
+
+SAVE_VOICE_PATH = './ApplePie/saveVoice/'
